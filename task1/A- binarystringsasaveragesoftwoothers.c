0d4b0fd829679efdb17c368416e86efd92c9cf31
PriyanshuDangi <priyanshudangipd@gmail.com>
2020-08-18 13:38:08 +0530

    removed the commented code

--- task1/A- binarystringsasaveragesoftwoothers.c
+++ task1/A- binarystringsasaveragesoftwoothers.c
@@ -40,33 +40,3 @@ int main() {
 	printf("%s", bin);
 	return 0;
 }
-
-
-//#include <stdio.h>
-//#include <string.h>
-//
-//int subtractone(char str[], int n){
-//	int m = 1;
-//	int i = n-1;
-//	int x = str[i] - '0';
-////	printf("%d\n", x&m);
-//	while(!(x&m) && i >= 0){
-//		str[i] = x ^ m + '0';
-//		printf("%c\n", str[i]);
-//		i--;
-//		x = str[i] - 48;
-//		m = 1;
-//	}
-//	printf("%s", str);
-//}
-//
-//int main(){
-//	int n;
-//	scanf("%d", &n);
-//	char str[n];
-//	scanf("%s", str);
-//	int result1[n];
-//	int result2[n];
-//	subtractone(str, strlen(str));
-////	printf("%s", result1);
-//}
